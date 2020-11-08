8f5a65f0d7583591a3eb1a4cd239143adb676c4a
penggs <pgs1108pgs@126.com>
2020-11-08 22:24:53 +0800

    practice：twoSum

---
+++ src/main/java/world/playtogether/hot/ApAddTwoNumbers.java
@@ -0,0 +1,77 @@
+package world.playtogether.hot;
+
+/**
+ * <project> algoPractice
+ *
+ * <p> 两数相加
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * @author penggs
+ * @since 2020-11-08
+ */
+public class ApAddTwoNumbers {
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 记录头节点
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            // 补零
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        // 最终还有进位，再加一个节点
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return head.next;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        ListNode(int x, ListNode next) {
+            this.val = x;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            ListNode p = this;
+            StringBuilder sb = new StringBuilder();
+            while (p != null) {
+                sb.append(p.val);
+                p = p.next;
+                if (p != null) sb.append(" -> ");
+            }
+
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        System.out.println(addTwoNumbers(l1, l2));
+    }
+}
---
+++ src/main/java/world/playtogether/hot/ApTwoSum.java
@@ -0,0 +1,39 @@
+package world.playtogether.hot;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <project> algoPractice
+ *
+ * <p> 两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标
+ *
+ * @author penggs
+ * @since 2020-11-08
+ */
+public class ApTwoSum {
+    /**
+     * 借助map缓存，时间复杂度o(n)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 4, 6, 3, 9, 20};
+        System.out.println(Arrays.toString(twoSum(arr, 13)));
+    }
+}
