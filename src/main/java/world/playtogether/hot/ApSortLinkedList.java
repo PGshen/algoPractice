441ae1fd517715daaff4d38d0e486a1653119b2e
penggs <pgs1108pgs@126.com>
2021-01-03 21:21:03 +0800

    链表排序

---
+++ src/main/java/world/playtogether/hot/ApSortLinkedList.java
@@ -0,0 +1,62 @@
+package world.playtogether.hot;
+
+/**
+ * <project> algoPractice
+ *
+ * <p> 对链表排序
+ *
+ * @author penggs
+ * @since 2021-01-03
+ */
+public class ApSortLinkedList {
+
+    /**
+     * 归并排序（递归）
+     * @param head
+     * @return
+     */
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
